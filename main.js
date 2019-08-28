bf3fb5708e1697eaa4d33651768c8263d17942a0
Michael Baumer <meesterbaumer@mac.com>
2019-08-27 20:25:46 -0500

    Added some stuff

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
 console.log('Hi!');
+
+const pies = [
+    { 
+    pieName: 'Apple',
+    image: 'https://images-gmi-pmc.edge-generalmills.com/173da066-c6b4-45dd-9b28-0d459cf6f169.jpg',
+    instructorName: 'zoe' 
+    },
+
+    {
+    pieName: 'Strawberry',
+    image: 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Easy-Fresh-Strawberry-Pie_EXPS_TMBBP19_34179_B06_20_3b.jpg',
+    instructorName: 'michael'
+    },
+
+    {
+    pieName: 'Chess',
+    image: 'https://shewearsmanyhats.com/wp-content/uploads/2014/11/chess-pie-2new.jpg',
+    instructorName: 'michael'
+    },
+
+    {
+    pieName: 'Shoefly',
+    image: 'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+    instructorName: 'Callan'
+    },
+
+    {
+    pieName: 'Whoopie',
+    image: 'https://assets.epicurious.com/photos/5892014d69bb8db572652044/master/pass/chocolate-whoopie-pies-020117.jpg',
+    instructorName: 'zoe'
+    },
+
+    {
+    pieName: 'Lemon',
+    image: 'https://lovingitvegan.com/wp-content/uploads/2017/04/Vegan-Lemon-Pie-14.jpg',
+    instructorName: 'zoe'
+    },
+
+];
+
+const printToDom = (toPrint, divId) => {
+    document.getElementById(divId).innerHTML += toPrint;
+    
+}
+
+const pieBuilder = (piesArray) => {
+    for (let i = 0; i < piesArray.length; i++) {
+        const pies = piesArray[i];
+        const domString = `
+        <div class="card">
+            <h2>${pies.pieName}</h2>
+            <img src=${pies.image} alt='Image of ${pies.pieName}' />
+        </div>
+        `
+        printToDom(domString, 'pie-zone')
+    }
+}
+
+pieBuilder(pies);
